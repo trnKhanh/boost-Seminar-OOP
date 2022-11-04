f54979adf14fecb316c2d4800727149d24ae6de5
trannamkhanh <tnk29082003@gmail.com>
2022-11-04 16:16:59 +0700

    Genaral update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
-#include <boost/container/stable_vector.hpp>
+#include <boost/container/small_vector.hpp>
 #include <iostream>
 #include <vector>
+#pragma pack(1)
 using namespace boost::container;
 using namespace std;
 int main()
 {
-    stable_vector<int> a1;
-    a1.push_back(0);
-    a1.push_back(1);
-    int *id1 = &a1[1];
-    cout << *id1 << endl;
-    for(int i=2;i<10;i++) a1.push_back(i);
-    cout << *id1 << endl;
+    small_vector<int, 10> a;
+    std::vector<int> b(2);
+    cout << sizeof(a) << endl;
+    for(int i=0; i < 100; ++i) a.push_back(i);
+    cout << a.size() << endl;
+    cout << sizeof(a) << endl;
 }
